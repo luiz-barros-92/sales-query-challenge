38275cbfab3b50b2081aed86a8e23d278d0a7e2c
Luiz Barros <lck.barros@gmail.com>
2025-08-20 23:48:09 -0300

    feat: create searchSaleSummary in SaleRepository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsmeta.repositories;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
@@ -18,4 +20,10 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 			+ "WHERE s.date BETWEEN :minDate AND :maxDate "
 			+ "AND UPPER(seller.name) LIKE UPPER(CONCAT('%', :sellerName, '%'))")
 	Page<SaleReportDTO> searchSaleReport(LocalDate minDate, LocalDate maxDate, String sellerName, Pageable pageable);
+	
+	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(s.name, SUM(sale.amount)) " 
+			+ "FROM Sale sale JOIN sale.seller s " 
+			+ "WHERE sale.date >= :minDate AND sale.date <= :maxDate " 
+			+ "GROUP BY s.name")
+	List<SaleSummaryDTO> searchSaleSummary(LocalDate minDate, LocalDate maxDate);	
 }
